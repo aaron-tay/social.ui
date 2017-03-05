1ce5d09ccfea255e241b12feb3a398cf07f52a5b
Aaron Tay <aaron-tay@users.noreply.github.com>
2017-03-05 16:16:16 +1100

    Added separate components for following and followers pages
    These are connected to vuex store but not yet connected to the routes
    This makes it easier to do things instead of the previous combining. There may be opportunities to combine at a different level but for now having them separated makes much more sense.

---
+++ src/components/ProfileFollowers.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="profile-collection">
+    <div class="columns is-mobile">
+      <div class="column">
+        <template v-for="person in personList">
+          <article class="media">
+            <figure class="media-left">
+              <p class="image is-64x64 sui-avatar">
+                <img :src="person.avatarUrl" />
+              </p>
+            </figure>
+            <div class="media-content">
+              <div class="content">
+                <p>
+                  <router-link :to="{ name: 'profile', params: { profileId: person.profileId }}">
+                    <strong>{{ person.name }}</strong>
+                  </router-link>
+                  <br />
+                  <span>
+                    {{ person.bio }}
+                  </span>
+                </p>
+              </div>
+            </div>
+            <div class="media-right">
+              <br />
+              <button class="button is-primary is-outlined" v-if="person.isFollowed" @click="unfollowPerson(person)">following</button>
+              <button class="button is-primary" v-else @click="followPerson(person)">follow</button>
+            </div>
+          </article>
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  name: 'profileFollowers',
+  props: ['person'],
+  data() {
+    return {
+    };
+  },
+  created() {
+    this.fetchUserList();
+  },
+  computed: {
+    ...mapGetters({
+      personList: 'activeFollowers',
+    }),
+  },
+  methods: {
+    ...mapActions([
+      'fetchUserFollowedByList',
+      'followUser',
+      'unfollowUser',
+    ]),
+    followPerson(person) {
+      this.followUser({
+        profileId: person.profileId,
+      });
+    },
+    unfollowPerson(person) {
+      this.unfollowUser({
+        profileId: person.profileId,
+      });
+    },
+    fetchUserList() {
+      this.fetchUserFollowedByList({
+        profileId: this.person.profileId,
+      });
+    },
+  },
+  watch: {
+    peopleListType(current, previous) {
+      if (current !== previous) {
+        this.fetchUserList();
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
